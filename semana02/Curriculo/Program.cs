801fab4cce0e169a64d8d17fb93acf34e445c79b
anjostiago_git <tiago1037@hotmail.com>
2026-09-12 14:07:31 -0300

    atualização 10

--- semana02/Curriculo/Program.cs
+++ semana02/Curriculo/Program.cs
@@ -3,15 +3,15 @@ public class Program
     public static void Main(string[] args)
     {
         Emprego emprego1 = new Emprego();
-        emprego1._empresa = "Microsoft";
+        emprego1._empresa = "Lenovo";
         emprego1._cargo = "Engenheiro de Software";
-        emprego1._anoInicio = 2019;
-        emprego1._anoFim = 2022;
+        emprego1._anoInicio = 2010;
+        emprego1._anoFim = 2024;
 
         Emprego emprego2 = new Emprego();
-        emprego2._empresa = "Apple";
-        emprego2._cargo = "Gerente";
-        emprego2._anoInicio = 2022;
+        emprego2._empresa = "IBM";
+        emprego2._cargo = "CEO";
+        emprego2._anoInicio = 2024;
         emprego2._anoFim = 2026;
 
         Curriculo meuCurriculo = new Curriculo();
