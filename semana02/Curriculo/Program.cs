4553591bca1729041692b1346a1297f06c89b338
anjostiago_git <tiago1037@hotmail.com>
2026-09-12 13:57:11 -0300

    atualização

--- semana02/Curriculo/Program.cs
+++ semana02/Curriculo/Program.cs
@@ -1,9 +1,25 @@
-using System;
-
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Curriculo.");
+        Emprego emprego1 = new Emprego();
+        emprego1._empresa = "Microsoft";
+        emprego1._cargo = "Engenheiro de Software";
+        emprego1._anoInicio = 2019;
+        emprego1._anoFim = 2022;
+
+        Emprego emprego2 = new Emprego();
+        emprego2._empresa = "Apple";
+        emprego2._cargo = "Gerente";
+        emprego2._anoInicio = 2022;
+        emprego2._anoFim = 2026;
+
+        Curriculo meuCurriculo = new Curriculo();
+        meuCurriculo._nome = "Tiago dos Anjos";
+
+        meuCurriculo._empregos.Add(emprego1);
+        meuCurriculo._empregos.Add(emprego2);
+
+        meuCurriculo.Exibir();
     }
 }
